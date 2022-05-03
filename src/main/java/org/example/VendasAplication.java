3a7e7404c412e4edcc195617a73f1a28147f22d4
PBarazetti <pebarazetti@gmail.com>
2022-05-03 15:51:19 -0300

    commit inicial

---
+++ src/main/java/org/example/VendasAplication.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendasAplication.class,args);
+    }
+}
