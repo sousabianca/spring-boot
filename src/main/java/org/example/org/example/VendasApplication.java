b31b3382a7793ec3e3c20d8a0734387c1d4c99db
PBarazetti <biamoon19@gmail.com>
2022-05-08 19:37:22 -0300

    CRUD

--- src/main/java/org/example/org/example/VendasApplication.java
+++ src/main/java/org/example/org/example/VendasApplication.java
@@ -1,2 +1,59 @@
-package org.example.org.example;public class VendasApplication {
+package org.example.org.example;
+
+import org.example.org.example.domain.entity.Cliente;
+import org.example.org.example.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init (@Autowired Clientes clientes){
+        return args -> {
+            System.out.println("Salvando clientes");
+            clientes.salvar(new Cliente("Bianca Sousa"));
+
+            clientes.salvar(new Cliente("Francisca Alves"));
+
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+
+            todosClientes.forEach(c->{
+                c.setNome(c.getNome()+ " atualizado.");
+                clientes.atualizar(c);
+            });
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+
+            clientes.buscarPorNome("Al").forEach(System.out::println);
+
+//            System.out.println("Deletando clientes");
+//            clientes.obterTodos().forEach(c->{
+//                clientes.deletar(c);
+//            });
+//
+//            todosClientes= clientes.obterTodos();
+//            if(todosClientes.isEmpty()){
+//                System.out.println("Nenhum cliente encontrado");
+//            }else{
+//                todosClientes.forEach(System.out::println);
+//            }
+
+
+        };
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class);
+    }
 }
--- src/main/java/org/example/org/example/domain/entity/Cliente.java
+++ src/main/java/org/example/org/example/domain/entity/Cliente.java
@@ -1,2 +1,45 @@
-package org.example.org.example.domain.entity;public class Cliente {
+package org.example.org.example.domain.entity;
+
+public class Cliente {
+
+
+    private Integer id;
+    private String nome;
+
+    public Cliente() {
+    }
+
+    public Cliente( String nome) {
+
+        this.nome = nome;
+    }
+
+    public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
